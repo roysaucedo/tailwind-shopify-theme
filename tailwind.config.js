3a426b8124e5682a51553dc724ca33b6676bdc9c
roysaucedo <roy.a.saucedo@gmail.com>
2022-09-22 14:49:35 -0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{liquid,json}"
+    // "./assets/*.liquid",
+    // "./layout/*.liquid",
+    // "./sections/*.liquid",
+    // "./snippets/*.liquid",
+    // "./templates/*.liquid",
+    // "./templates/customers/*.liquid"
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
